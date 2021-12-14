f695c10873a2fe3edec44ddb13dcb9662d3df721
Gang Zhang <gzhangx@hotmail.com>
2021-12-13 22:19:46 -0500

    fonts and graphs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global.css'
 import '../styles/sb-admin-2.css'
+import '../public/fontawesome-free/css/all.css'
 import Head from 'next/head'
 export default function App({ Component, pageProps }) {    
     return <Component {...pageProps} />
