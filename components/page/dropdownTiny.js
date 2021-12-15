11b33efc3531860dcc8b7df1eb7458958a5b0d27
Gang Zhang <gzhangx@hotmail.com>
2021-12-15 16:14:03 -0500

    refactors

---
+++ components/page/dropdownTiny.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+export default function DropdownTiny(props) {
+    const [state, setState] = useState(false);
+    const className = `dropdown-menu dropdown-menu-right shadow animated--fade-in ${state && 'show'}`;
+    return <div className="dropdown no-arrow">
+        <a className="dropdown-toggle" href="#" role="button"
+            onClick={e => {
+                e.preventDefault();
+                setState(!state);
+            }}
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+        </a>
+        <div className={className}
+            aria-labelledby="dropdownMenuLink">
+            {
+                props.children
+            }
+        </div>
+    </div>
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,6 +6,7 @@ import {EarningsGraph, RevenueSourceGraph } from '../components/page/earningsGra
 import { TopBar } from '../components/page/topbar'
 import { Footer } from '../components/page/pageFooter'
 import BoardItemHalfSmall from '../components/page/boardItemHaflSmall'
+import DropdownTiny from '../components/page/dropdownTiny'
 export default function MainDashboard(props) {
   const { state, setMainState } = props;
   const pstate = useState({
@@ -70,20 +71,14 @@ export default function MainDashboard(props) {
                     <div
                       className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                       <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                      <div className="dropdown no-arrow">
-                        <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                          aria-labelledby="dropdownMenuLink">
-                          <div className="dropdown-header">Dropdown Header:</div>
-                          <a className="dropdown-item" href="#">Action</a>
-                          <a className="dropdown-item" href="#">Another action</a>
-                          <div className="dropdown-divider"></div>
-                          <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                      </div>
+                      <DropdownTiny>
+                        <>
+                        <div className="dropdown-header">Dropdown Header:</div>
+                        <a className="dropdown-item" href="#">Load</a>                        
+                        <div className="dropdown-divider"></div>
+                          <a className="dropdown-item" href="#">Refresh</a>
+                        </>
+                      </DropdownTiny>
                     </div>
                     <div className="card-body">
                       <div className="chart-area">
