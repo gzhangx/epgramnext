a8415dc0fb71957dc70239f5624125371bff958f
Gang Zhang <gzhangx@hotmail.com>
2021-12-14 21:58:52 -0500

    refactor sidebar

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -13,7 +13,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line, Pie } from 'react-chartjs-2';
-import { PageNavTab} from '../components/page/navTab';
+import {MainSideBar} from '../components/page/sidebar'
 
 ChartJS.register(
   CategoryScale,
@@ -57,20 +57,8 @@ export default function MainDashboard(props) {
     sideBarComponentExpanded: false,
     sideBarUtilitiesExpanded: false,
   });
-  const [pageState, setPageState] = pstate;
-  const getSideBarKey = name => `sideBar${name}Expanded`;
-  const toggleSideBar = name => {
-    const key = getSideBarKey(name);
-    const val = pageState[key];
-    setPageState({
-      ...pageState,
-      [key]: !val,
-    })
-  };
-  const getSideBarState = name => {
-    const key = getSideBarKey(name);
-    return pageState[key];
-  }
+  //const [pageState, setPageState] = pstate;
+  
   const defData = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
@@ -134,135 +122,7 @@ export default function MainDashboard(props) {
     
     <div id="page-top">
       <div id="wrapper">
-        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-          <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div className="sidebar-brand-icon rotate-n-15">
-              <i className="fas fa-tractor"></i>
-            </div>
-            <div className="sidebar-brand-text mx-3">Equipment Gram <sup>tm</sup></div>
-          </a>
-
-          <hr className="sidebar-divider my-0" />
-
-          <li className="nav-item active">
-            <a className="nav-link" href="index.html" onClick={e => e.preventDefault()}>
-              <i className="fas fa-fw fa-tachometer-alt"></i>
-              <span>Dashboard</span></a>
-          </li>
-
-          <hr className="sidebar-divider" />
-
-          <div className="sidebar-heading">
-            Interface
-          </div>
-           
-          <PageNavTab name="Components"
-            pstate={pstate}
-            header={
-              <>
-                <i className="fas fa-fw fa-cog"></i>
-                <span>Components</span>
-              </>
-            }
-            body={
-              <>
-                <h6 className="collapse-header">Custom Components:</h6>
-                <a className="collapse-item" href="buttons.html">Buttons</a>
-                <a className="collapse-item" href="cards.html">Cards</a>
-              </>
-            }
-          ></PageNavTab>
-          
-          <PageNavTab name="Utilities"
-            pstate={pstate}
-            header={
-              <>
-                <a>
-                  <i className="fas fa-fw fa-wrench"></i>
-                  <span>Utilities</span>
-                </a>
-              </>
-            }
-            body={
-              <>
-                <h6 className="collapse-header">Custom Utilities:</h6>
-                <a className="collapse-item" href="utilities-color.html">Colors</a>
-                <a className="collapse-item" href="utilities-border.html">Borders</a>
-                <a className="collapse-item" href="utilities-animation.html">Animations</a>
-                <a className="collapse-item" href="utilities-other.html">Other</a>
-              </>
-            }
-          ></PageNavTab>
-          <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-              aria-expanded="true" aria-controls="collapseUtilities">
-              <i className="fas fa-fw fa-wrench"></i>
-              <span>Utilities</span>
-            </a>
-            <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
-              data-parent="#accordionSidebar">
-              <div className="bg-white py-2 collapse-inner rounded">
-                <h6 className="collapse-header">Custom Utilities:</h6>
-                <a className="collapse-item" href="utilities-color.html">Colors</a>
-                <a className="collapse-item" href="utilities-border.html">Borders</a>
-                <a className="collapse-item" href="utilities-animation.html">Animations</a>
-                <a className="collapse-item" href="utilities-other.html">Other</a>
-              </div>
-            </div>
-          </li>
-
-          <hr className="sidebar-divider" />
-
-          <div className="sidebar-heading">
-            Addons
-          </div>
-
-          <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-              aria-expanded="true" aria-controls="collapsePages">
-              <i className="fas fa-fw fa-folder"></i>
-              <span>Pages</span>
-            </a>
-            <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-              <div className="bg-white py-2 collapse-inner rounded">
-                <h6 className="collapse-header">Login Screens:</h6>
-                <a className="collapse-item" href="login.html">Login</a>
-                <a className="collapse-item" href="register.html">Register</a>
-                <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div className="collapse-divider"></div>
-                <h6 className="collapse-header">Other Pages:</h6>
-                <a className="collapse-item" href="404.html">404 Page</a>
-                <a className="collapse-item" href="blank.html">Blank Page</a>
-              </div>
-            </div>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <i className="fas fa-fw fa-chart-area"></i>
-              <span>Charts</span></a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <i className="fas fa-fw fa-table"></i>
-              <span>Tables</span></a>
-          </li>
-
-          <hr className="sidebar-divider d-none d-md-block" />
-
-          <div className="text-center d-none d-md-inline">
-            <button className="rounded-circle border-0" id="sidebarToggle"></button>
-          </div>
-
-          <div className="sidebar-card d-none d-lg-flex">
-            <img className="sidebar-card-illustration mb-2" src="/img/undraw_rocket.svg" alt="..." layout='fixed' width={100} height={100} />
-            <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-            <a className="btn btn-success btn-sm" href="#">Upgrade to Pro!</a>
-          </div>
-
-        </ul>
+        <MainSideBar pstate={pstate}></MainSideBar>
 
         <div id="content-wrapper" className="d-flex flex-column">
 
